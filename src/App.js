6889f1578c1bfc8da9336f213e81dc6e41db36c2
Anjan <anjanlayek001@gmail.com>
2022-11-20 18:56:37 +0530

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,35 @@
 import logo from './logo.svg';
 import './App.css';
+import {useState} from 'react'
 
 function App() {
+  const [text,setText] = useState('');
+  const[list,setList] = useState([]);
+  const listUpdater = ()=>{
+    setList((item) => {
+      return [...item,text]
+    })
+    setText('')
+  }
+  const deleteList = ((e) =>{
+    setList((items)=>{
+     return items.filter((elmt,i)=>{
+        return e.target.id != i
+      })
+    })
+  })
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+     <input type='text' placeholder='Enter Text'value={text} onChange={(e)=>  {setText(e.target.value)}}></input>
+     <button onClick={listUpdater}>Add</button>
+     
+     <div>
+      <ol>
+        {list.map((e,i) => <><li key={i}>{e}</li>
+        <button onClick={deleteList} id={i}>Delete</button>
+        </>)}
+        </ol>
+        </div>
     </div>
   );
 }
